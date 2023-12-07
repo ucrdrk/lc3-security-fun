f671a3f62c3f842df45ec43be54b7b79c53df49c
Allan Knight <67027104+ucrdrk@users.noreply.github.com>
2023-12-06 18:00:43 -0800

    Added C++ file for stack pointer stuff

---
+++ stack_pointer.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+
+int * return_stack_ptr(int val) {
+    int i = val;
+    std::cout << &i << std::endl;
+    return &i;
+}
+
+int &return_stack_ref(int val) {
+    int i = val;
+    return i;
+}
+
+int *return_heap_ptr(int val) {
+    auto ptr = new int(val);
+    return ptr;
+}
+
+int main (int argc, char *argv[]) {
+    int *int_heap = return_heap_ptr(9);
+    std::cout << *int_heap << std::endl;
+    int *int_ptr = return_stack_ptr(10);
+    std::cout << int_ptr << std::endl;
+    std::cout << &return_stack_ref(11) << std::endl;
+    return EXIT_SUCCESS;
+}
